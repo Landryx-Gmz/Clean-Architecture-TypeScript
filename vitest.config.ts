c4f1bf66bc9f5b575347ca158f75d567d10ab8c3
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-12-17 09:13:12 +0100

    mejora: actualizar configuraciones de tsconfig y vitest para alias de rutas

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    resolve: {
+        alias: [
+            { find: '@domain', replacement: path.resolve(__dirname, 'src/domain') },
+            { find: '@application', replacement: path.resolve(__dirname, 'src/application') },
+            { find: '@aplication', replacement: path.resolve(__dirname, 'src/application') },
+            { find: '@infrastructure', replacement: path.resolve(__dirname, 'src/infrastructure') },
+            { find: '@infrastructura', replacement: path.resolve(__dirname, 'src/infrastructure') },
+            { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
+            { find: '@share', replacement: path.resolve(__dirname, 'src/shared') },
+            { find: '@composition', replacement: path.resolve(__dirname, 'src/composition') }
+        ]
+    },
     test: {
         globals: true,
         environment: 'node',
